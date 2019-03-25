f316fe653fa743edfa7bcf60978cfdc8d2cfb4b1
Renan Tomazoni Pinzon <rpinzon@gmail.com>
2019-03-25 00:43:52 -0300

    Add printer for matrix

---
+++ src/main/java/org/rpinzon/indorse/util/Printer.java
@@ -0,0 +1,43 @@
+package org.rpinzon.indorse.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by Renan T. Pinzon on 24/03/19.
+ */
+public class Printer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Printer.class);
+
+    private Printer() {
+        // to prevent this class from being explicitly instantiated
+    }
+
+    /**
+     * Print all elements of the given matrix separating elements in the same
+     * row by space.
+     * <p/>
+     * Example:
+     * <pre>
+     *   2 9 1
+     *   3 -1 4
+     *   4 0 8
+     * </pre>
+     *
+     * @param matrix the matrix to print values
+     */
+    public static void display(int[][] matrix) {
+        StringBuilder output = new StringBuilder();
+        for (int[] row : matrix) {
+            for (int col : row) {
+                // append each element of a row separated by space
+                output.append(col).append(" ");
+            }
+            // break line
+            output.append('\n');
+        }
+        LOG.info(output.toString());
+    }
+
+}
